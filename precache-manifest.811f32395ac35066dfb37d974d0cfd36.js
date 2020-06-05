7f63439d000e7f50ecb98ef771d60120a10c51fe
iprowriter <46689083+iprowriter@users.noreply.github.com>
2020-06-05 10:43:01 +0200

    Updates

---
+++ precache-manifest.811f32395ac35066dfb37d974d0cfd36.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ea54c851db18eb3b52a37b31148448bf",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "2d2fe1b30a8b159763c2",
+    "url": "/react-expense-tracker/static/css/main.253372c2.chunk.css"
+  },
+  {
+    "revision": "52be1f5d5c695c93f168",
+    "url": "/react-expense-tracker/static/js/2.425c6326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.425c6326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2fe1b30a8b159763c2",
+    "url": "/react-expense-tracker/static/js/main.773b1978.chunk.js"
+  },
+  {
+    "revision": "4752dbf25a34323b5b7c",
+    "url": "/react-expense-tracker/static/js/runtime-main.38b51ddc.js"
+  }
+]);
